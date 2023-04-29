df6256d2163d05280abf15e3a1a92dcf15fde45d
RolandMarton <roland111187@gmail.com>
2023-04-29 15:41:32 +0200

    Initalize demo project to practise flutter

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: NinjaCard(),
+  ));
+}
+
+class NinjaCard extends StatelessWidget {
+  const NinjaCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Ninja ID Card'),
+      ),
+    );
+  }
+
+}
+
